60019bf1b4eec1f420836395f38423aa9a2d3ffa
v4nderstruck <v4nderstruck@gmail.com>
2025-12-21 12:34:01 +0100

    clibs: use singe cImport

--- src/vk/clibs.zig
+++ src/vk/clibs.zig
@@ -1,32 +1,31 @@
 const std = @import("std");
-const sdl_c = @cImport({
+const cInclude = @cImport({
     @cInclude("SDL3/SDL.h");
     @cInclude("SDL3/SDL_vulkan.h");
-});
-
-const vulkan_c = @cImport({
     @cInclude("vulkan/vulkan.h");
 });
 
 const sdl_scoped_log = std.log.scoped(.sdl3);
 pub const sdl = struct {
     // Structs
-    pub const Window = sdl_c.SDL_Window;
-    pub const WindowFlags = sdl_c.SDL_WindowFlags;
+    pub const Window = cInclude.SDL_Window;
+    pub const WindowFlags = cInclude.SDL_WindowFlags;
+    pub const VulkanSurface = cInclude.VkSurfaceKHR;
 
     // Functions/Macros
-    pub const Init = sdl_c.SDL_Init;
-    pub const CreateWindow = sdl_c.SDL_CreateWindow;
-    pub const DestroyWindow = sdl_c.SDL_DestroyWindow;
+    pub const Init = cInclude.SDL_Init;
+    pub const CreateWindow = cInclude.SDL_CreateWindow;
+    pub const DestroyWindow = cInclude.SDL_DestroyWindow;
 
-    pub const GetError = sdl_c.SDL_GetError;
-    pub const VulkanGetInstanceExtension = sdl_c.SDL_Vulkan_GetInstanceExtensions;
-    pub const VulkanGetVkInstanceProcAddr = sdl_c.SDL_Vulkan_GetVkGetInstanceProcAddr;
+    pub const GetError = cInclude.SDL_GetError;
+    pub const VulkanGetInstanceExtension = cInclude.SDL_Vulkan_GetInstanceExtensions;
+    pub const VulkanGetVkInstanceProcAddr = cInclude.SDL_Vulkan_GetVkGetInstanceProcAddr;
+    pub const VulkanCreateSurface = cInclude.SDL_Vulkan_CreateSurface;
 
     // Constants
-    pub const INIT_VIDEO = sdl_c.SDL_INIT_VIDEO;
-    pub const WINDOW_VULKAN = sdl_c.SDL_WINDOW_VULKAN;
-    pub const WINDOW_RESIZABLE = sdl_c.SDL_WINDOW_RESIZABLE;
+    pub const INIT_VIDEO = cInclude.SDL_INIT_VIDEO;
+    pub const WINDOW_VULKAN = cInclude.SDL_WINDOW_VULKAN;
+    pub const WINDOW_RESIZABLE = cInclude.SDL_WINDOW_RESIZABLE;
 
     // Helpers
     pub fn sdl_try(result: c_int) void {
@@ -40,54 +39,56 @@ pub const sdl = struct {
 const vk_scoped_log = std.log.scoped(.vulkan);
 pub const vk = struct {
     // Structs/Types
-    pub const ApplicationInfo = vulkan_c.VkApplicationInfo;
-    pub const InstanceCreateInfo = vulkan_c.VkInstanceCreateInfo;
-    pub const Instance = vulkan_c.VkInstance;
-    pub const Bool32 = vulkan_c.VkBool32;
-    pub const DebugUtilsMessengerEXT = vulkan_c.VkDebugUtilsMessengerEXT;
-    pub const DebugUtilsMessengerCreateInfoEXT = vulkan_c.VkDebugUtilsMessengerCreateInfoEXT;
-    pub const DebugMessageSeverityFlagBitsExt = vulkan_c.VkDebugUtilsMessageSeverityFlagBitsEXT;
-    pub const DebugMessageTypeFlagsExt = vulkan_c.VkDebugUtilsMessageTypeFlagsEXT;
-    pub const DebugMessengerCallbackDataExt = vulkan_c.VkDebugUtilsMessengerCallbackDataEXT;
-    pub const PhysicalDevice = vulkan_c.VkPhysicalDevice;
-    pub const PhysicalDeviceProperties = vulkan_c.VkPhysicalDeviceProperties;
+    pub const ApplicationInfo = cInclude.VkApplicationInfo;
+    pub const InstanceCreateInfo = cInclude.VkInstanceCreateInfo;
+    pub const Instance = cInclude.VkInstance;
+    pub const Bool32 = cInclude.VkBool32;
+    pub const DebugUtilsMessengerEXT = cInclude.VkDebugUtilsMessengerEXT;
+    pub const DebugUtilsMessengerCreateInfoEXT = cInclude.VkDebugUtilsMessengerCreateInfoEXT;
+    pub const DebugMessageSeverityFlagBitsExt = cInclude.VkDebugUtilsMessageSeverityFlagBitsEXT;
+    pub const DebugMessageTypeFlagsExt = cInclude.VkDebugUtilsMessageTypeFlagsEXT;
+    pub const DebugMessengerCallbackDataExt = cInclude.VkDebugUtilsMessengerCallbackDataEXT;
+    pub const PhysicalDevice = cInclude.VkPhysicalDevice;
+    pub const PhysicalDeviceProperties = cInclude.VkPhysicalDeviceProperties;
+    pub const QueueFamilyProperties = cInclude.VkQueueFamilyProperties;
 
-    pub const PFN_CreateDebugUtilsMessagerExt = vulkan_c.PFN_vkCreateDebugUtilsMessengerEXT;
-    pub const PFN_GetInstanceProcAddr = vulkan_c.PFN_vkGetInstanceProcAddr;
+    pub const PFN_CreateDebugUtilsMessagerExt = cInclude.PFN_vkCreateDebugUtilsMessengerEXT;
+    pub const PFN_GetInstanceProcAddr = cInclude.PFN_vkGetInstanceProcAddr;
 
     // Functions/Macros
-    pub const DestroyInstance = vulkan_c.vkDestroyInstance;
-    pub const CreateInstance = vulkan_c.vkCreateInstance;
-    pub const GetInstanceProcAddr = vulkan_c.vkGetInstanceProcAddr;
-    pub const MAKE_VERSION = vulkan_c.VK_MAKE_VERSION;
-    pub const EnumeratePhysicalDevices = vulkan_c.vkEnumeratePhysicalDevices;
-    pub const GetPhysicalDeviceProperties = vulkan_c.vkGetPhysicalDeviceProperties;
+    pub const DestroyInstance = cInclude.vkDestroyInstance;
+    pub const CreateInstance = cInclude.vkCreateInstance;
+    pub const GetInstanceProcAddr = cInclude.vkGetInstanceProcAddr;
+    pub const MAKE_VERSION = cInclude.VK_MAKE_VERSION;
+    pub const EnumeratePhysicalDevices = cInclude.vkEnumeratePhysicalDevices;
+    pub const GetPhysicalDeviceProperties = cInclude.vkGetPhysicalDeviceProperties;
+    pub const GetPhysicalDeviceQueueFamiliyProperties = cInclude.vkGetPhysicalDeviceQueueFamilyProperties;
 
     // Constants
-    pub const API_VERSION = vulkan_c.VK_API_VERSION_1_3;
-    pub const STRUCTURE_TYPE_APPLICATION_INFO = vulkan_c.VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    pub const STRUCTURE_TYPE_INSTANCE_CREATE_INFO = vulkan_c.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    pub const STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT = vulkan_c.VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-    pub const EXT_DEBUG_UTILS = vulkan_c.VK_EXT_debug_utils;
-    pub const EXT_DEBUG_UTILS_EXTENSION_NAME = vulkan_c.VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
+    pub const API_VERSION = cInclude.VK_API_VERSION_1_3;
+    pub const STRUCTURE_TYPE_APPLICATION_INFO = cInclude.VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    pub const STRUCTURE_TYPE_INSTANCE_CREATE_INFO = cInclude.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    pub const STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT = cInclude.VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    pub const EXT_DEBUG_UTILS = cInclude.VK_EXT_debug_utils;
+    pub const EXT_DEBUG_UTILS_EXTENSION_NAME = cInclude.VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
     pub const VAL_LAYER_KHRONOS_VALIDATION = "VK_LAYER_KHRONOS_validation"; // Not in headers?
-    pub const BoolFalse = vulkan_c.VK_FALSE;
-    pub const BoolTrue = vulkan_c.VK_TRUE;
-    pub const NullHandle = vulkan_c.VK_NULL_HANDLE;
+    pub const BoolFalse = cInclude.VK_FALSE;
+    pub const BoolTrue = cInclude.VK_TRUE;
+    pub const NullHandle = cInclude.VK_NULL_HANDLE;
 
     // Derived Constants
-    pub const ZD_DEBUG_MESSAGE_SEVERITY_INFO = vulkan_c.VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT |
-        vulkan_c.VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
-        vulkan_c.VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT;
-    pub const ZD_DEBUG_MESSAGE_SEVERITY_DEBUG = vulkan_c.VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT |
-        vulkan_c.VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
-        vulkan_c.VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT |
-        vulkan_c.VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT;
-    pub const ZD_DEBUG_MESSAGE_TYPE = vulkan_c.VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
-        vulkan_c.VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
-        vulkan_c.VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    pub const ZD_DEBUG_MESSAGE_SEVERITY_INFO = cInclude.VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT |
+        cInclude.VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+        cInclude.VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT;
+    pub const ZD_DEBUG_MESSAGE_SEVERITY_DEBUG = cInclude.VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT |
+        cInclude.VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+        cInclude.VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT |
+        cInclude.VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT;
+    pub const ZD_DEBUG_MESSAGE_TYPE = cInclude.VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+        cInclude.VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+        cInclude.VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
 
-    pub const SUCCESS = vulkan_c.VK_SUCCESS;
+    pub const SUCCESS = cInclude.VK_SUCCESS;
 
     // Vulkan stuff
     pub fn getVulkanInstanceFnByName(comptime Fn: type, instance: Instance, name: [:0]const u8) Fn {
